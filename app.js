20c841f7430c2a40b3cab7f3a12831ef9e3b2237
thebolarin <odutusinmoses@gmail.com>
2020-04-12 23:15:48 +0100

    prepared for deployment

--- app.js
+++ app.js
@@ -5,14 +5,28 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const morgan = require('morgan');
+const helmet= require('helmet');
+const compression = require('compression');
 const movieRoutes = require('./route/movies')
 
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname,'access.log'),
+  {flags:'a'}
+);
+
 const app = express();
+app.use (helmet());
 app.use(cors());
+app.use(compression());
+app.use(morgan('combined' , {stream:accessLogStream}));
 
-const MONGODB_URI = 'mongodb+srv://bolarinwa:P8yOmHjo17w9UcO4@cluster0-25yze.mongodb.net/checkdc?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
 
+// const MONGODB_URI = 'mongodb+srv://bolarinwa:P8yOmHjo17w9UcO4@cluster0-25yze.mongodb.net/checkdc?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
 
+const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${
+  process.env.MONGO_PASSWORD
+}@cluster0-25yze.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`;
 
 app.use(bodyParser.json()); // application/json
 
